5e12268e638419089d5f9ee15f072121ccbde6c2
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-17 19:19:38 +0100

    [feat] button function

---
+++ src/components/Languages/Button.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import [ useState]
+
+export default function Button() {
+    return (
+        <section className="description-card">
+        <h2 className="title">Title</h2>
+        <p className="description">Text</p>
+    </section>
+    )
+
+}
