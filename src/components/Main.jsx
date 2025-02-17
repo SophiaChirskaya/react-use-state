818ee97abfb7348e6b3556bac37b8494516e319d
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-17 18:20:17 +0100

    [feat] Main

---
+++ src/components/Main.jsx
@@ -0,0 +1,22 @@
+export default function Main() {
+    return (
+        <main>
+            <nav>
+                <ul>
+                    <li><a href="">HTML</a></li>
+                    <li><a href="">CSS</a></li>
+                    <li><a href="">JavaScript</a></li>
+                    <li><a href="">Node.js</a></li>
+                    <li><a href="">Express</a></li>
+                    <li><a href="">ReactJS</a></li>
+                </ul>
+            </nav>
+            <section className="description">
+                <h2>Title</h2>
+                <p>Text</p>
+
+            </section>
+        </main>
+    );
+    
+}
