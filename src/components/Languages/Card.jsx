6c9f7da2c768c447d013eb448cd5d6a2ba881b94
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-18 16:03:56 +0100

    [feat]Card.jsx

---
+++ src/components/Languages/Card.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Card() {
+    return (
+        <div className="card">
+        <h3 className="card-title"></h3>
+        <p className="description"></p>
+    </div>
+        
+
+    );
+
+};
